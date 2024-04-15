34abac36a153e6dfff841736d9a208f58a1cbbde
Mahmoud_Mohammad <magma.m66@gmail.com>
2024-04-15 12:11:32 +0200

    response toegevoegt en een branch aangemaakt

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+        $response->assertSee(value:'Documentation');
+
         $response->assertStatus(200);
     }
 }
